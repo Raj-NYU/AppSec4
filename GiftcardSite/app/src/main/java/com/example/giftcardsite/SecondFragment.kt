ce4db7eaafcbe351a9485004e03990f1820672b1
RajShah <ps4341@nyu.edu>
2023-05-08 10:20:16 -0400

    part_2_complete

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
@@ -66,7 +66,8 @@ class SecondFragment : Fragment() {
                         loggedInUser = response.body()
                         Log.d("Register Success", "Register success. Boo.")
                         Log.d("Register Success", "Token:" + loggedInUser?.token.toString())
-                        var intent = Intent(Intent.ACTION_VIEW)
+                        // Part 2.2: Removing implicit intent and adding explicit intent
+                        var intent = Intent(activity, ProductScrollingActivity::class.java)
                         intent.type = "text/giftcards_browse"
                         intent.data = Uri.parse("https://appsec.moyix.net/api/index")
                         intent.putExtra("User", loggedInUser);
